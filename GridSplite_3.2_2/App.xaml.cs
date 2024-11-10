f7dc16340e046b2e7f6a5ff6cd2553e1e6011c1b
Dedukhin Alexandr <vyshky2@gmail.com>
2024-11-10 23:28:49 +0400

    init

---
+++ GridSplite_3.2_2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GridSplite_3._2_2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
